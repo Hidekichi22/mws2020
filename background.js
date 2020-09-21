5bdbeb257b05a0db660a402407781c1cbf74d60b
tubutubucorn <33154559+tubutubucorn@users.noreply.github.com>
2020-09-21 13:50:09 +0900

    add gmail

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
-async function html_filter(requestDetails){
-    let url = requestDetails.originUrl;
-    if (url.split('/')[2] == 'mail.google.com'){ // 遷移前のURLをとってくる
+function html_filter(requestDetails){
+    let link_list = [];
+    console.log(requestDetails.url);
 
+    // Gmailへの遷移だったらメール上のリンクをとってくる
+    if (requestDetails.url.split('/')[2] == 'mail.gmail.com'){
         let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
-        let encoder = new TextEncoder();
         let decoder = new TextDecoder('utf-8');
         let data = [];
 
@@ -11,7 +12,32 @@ async function html_filter(requestDetails){
             data.push(event.data);    // データをスタックに積む
             filter.write(event.data); // データをブラウザにわたす
         }
+        filter.onstop = event => {    // データをすべて受け取り終わったら            // スタックに積んだデータを文字列にデコード
+            let str = '';
+            for (let buffer of data) {
+                str += decoder.decode(buffer, {stream: true});
+            }
+            str += decoder.decode(); // end-of-stream
 
+            // 空白, 改行を改行に
+            str = str.replace(/(\s+|\r\n|\n|\r|\"|\')/gm,'\n');
+            // URLを取得
+            link_list = str.match(/https+.*/gm);
+            console.log(link_list);
+            filter.close();           // フィルタオブジェクトを終了する
+        }
+    }
+
+    // 遷移後がGmailでなく、遷移前がGmailだったらフィルタ処理
+    else if (requestDetails.originUrl.split('/')[2] == 'mail.gmail.com' && link_list.includes(requestDetails.url)){
+        let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
+        let decoder = new TextDecoder('utf-8');
+        let data = [];
+
+        filter.ondata = event => {    // データを受け取ったら(パケットに小分けされて何回も受け取る)
+            data.push(event.data);    // データをスタックに積む
+            filter.write(event.data); // データをブラウザにわたす
+        }
         filter.onstop = event => {    // データをすべて受け取り終わったら            // スタックに積んだデータを文字列にデコード
             let str = '';
             for (let buffer of data) {
@@ -22,43 +48,37 @@ async function html_filter(requestDetails){
             // 空白, 改行を削除
             str = str.replace(/(\s+|\r\n|\n|\r)/gm,'');
             // headタグを抽出
-            str = str.match(/<head>.*<\/head>/);
+            let head = str.match(/<head>.*<\/head>/);
             // headタグがあれば次の処理に遷移
-            if (str != null) {
-                str = str[0];
-                // ここに機能を追加する
-                console.log(str);
+            if (head != null) {
+                head = head[0];
+                // ここにフィルタ処理を追加する
+                console.log(head);
             }
-
             filter.close();           // フィルタオブジェクトを終了する
         }
 
         // 証明書
-        try {
-            let securityInfo = await browser.webRequest.getSecurityInfo(
-                requestDetails.requestId,
-                {}
-            )
-            console.log(securityInfo)
-        }
-        catch(error){
-            console.error(error);
-        }
-
+        browser.webRequest.onHeadersReceived.addListener(
+            async function(requestDetails){
+                try {
+                    let securityInfo = await browser.webRequest.getSecurityInfo(
+                        requestDetails.requestId,
+                        {}
+                    )
+                    console.log(securityInfo)
+                }
+                catch(error){
+                    console.error(error);
+                }
+            },
+            {urls:['*://*/*'], types:['main_frame']}
+        )
     }
+
     else{
         // gmail以外からだった時の処理(テスト用)
-        console.log(url.split('/')[2]);
-        try {
-            let securityInfo = await browser.webRequest.getSecurityInfo(
-                requestDetails.requestId,
-                {}
-            )
-            console.log(securityInfo)
-        }
-        catch(error){
-            console.error(error);
-        }
+        console.log(requestDetails.url.split('/')[2]);
     }
 }
 
