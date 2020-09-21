6d075062e4fff7e1d3468c6dd961fc7d55c70fb9
tubutubucorn <33154559+tubutubucorn@users.noreply.github.com>
2020-09-21 12:03:57 +0900

    add mail check and cert check

--- background.js
+++ background.js
@@ -1,52 +1,73 @@
-function html_filter(requestDetails){
-    let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
-    let encoder = new TextEncoder();
-    let decoder = new TextDecoder('utf-8');
-    let data = [];
+async function html_filter(requestDetails){
+    let url = requestDetails.originUrl;
+    if (url.split('/')[2] == 'mail.google.com'){ // 遷移前のURLをとってくる
 
-    // 遷移前のURLをとってくる
-    console.log(requestDetails.originUrl);
+        let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
+        let encoder = new TextEncoder();
+        let decoder = new TextDecoder('utf-8');
+        let data = [];
 
-    filter.ondata = event => {    // データを受け取ったら(パケットに小分けされて何回も受け取る)
-        data.push(event.data);    // データをスタックに積む
-        filter.write(event.data); // データをブラウザにわたす
-    }
-
-    filter.onstop = event => {    // データをすべて受け取り終わったら
-        // スタックに積んだデータを文字列にデコード
-        let str = '';
-        for (let buffer of data) {
-            str += decoder.decode(buffer, {stream: true});
-        }
-        str += decoder.decode(); // end-of-stream
-
-        // 空白, 改行を削除
-        str = str.replace(/(\s+|\r\n|\n|\r)/gm,'');
-        // headタグを抽出
-        str = str.match(/<head>.*<\/head>/);
-        // headタグがあれば次の処理に遷移
-        if (str != null) {
-            str = str[0];
-            // ここに機能を追加する
-            console.log(str);
+        filter.ondata = event => {    // データを受け取ったら(パケットに小分けされて何回も受け取る)
+            data.push(event.data);    // データをスタックに積む
+            filter.write(event.data); // データをブラウザにわたす
         }
 
-        filter.close();           // フィルタオブジェクトを終了する
-    }
+        filter.onstop = event => {    // データをすべて受け取り終わったら            // スタックに積んだデータを文字列にデコード
+            let str = '';
+            for (let buffer of data) {
+                str += decoder.decode(buffer, {stream: true});
+            }
+            str += decoder.decode(); // end-of-stream
 
-    let securityInfo = webRequest.getSecurityInfo(requestDetails.requestId, certificateChain=True);
-    console.log(securityInfo.certificates[securityInfo.certificates.length - 1].issuer);
-    // 証明書関係 参考
-    // https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/getSecurityInfo
-    // https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/CertificateInfo
-    //console.log(browser.getSecurityInfo(requestDetails.requestId))
+            // 空白, 改行を削除
+            str = str.replace(/(\s+|\r\n|\n|\r)/gm,'');
+            // headタグを抽出
+            str = str.match(/<head>.*<\/head>/);
+            // headタグがあれば次の処理に遷移
+            if (str != null) {
+                str = str[0];
+                // ここに機能を追加する
+                console.log(str);
+            }
+
+            filter.close();           // フィルタオブジェクトを終了する
+        }
+
+        // 証明書
+        try {
+            let securityInfo = await browser.webRequest.getSecurityInfo(
+                requestDetails.requestId,
+                {}
+            )
+            console.log(securityInfo)
+        }
+        catch(error){
+            console.error(error);
+        }
+
+    }
+    else{
+        // gmail以外からだった時の処理(テスト用)
+        console.log(url.split('/')[2]);
+        try {
+            let securityInfo = await browser.webRequest.getSecurityInfo(
+                requestDetails.requestId,
+                {}
+            )
+            console.log(securityInfo)
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
 }
+
 
 browser.webRequest.onBeforeRequest.addListener(
     // callback: このイベントが発生したときに呼び出される関数
     html_filter,
     // filter: このリスナーに送信されるイベントを制限するフィルタ
-    {urls:['*://*/*'],types:['main_frame']},
+    {urls:['*://*/*'], types:['main_frame']},
     // blocking: リクエストを同期化して、リクエストをキャンセルまたはリダイレクトできるようにする
     ['blocking']
 );
